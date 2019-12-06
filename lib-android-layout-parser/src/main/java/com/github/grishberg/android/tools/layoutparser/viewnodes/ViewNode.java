564a73c96b63baf3f4e0f0ae07cd15e7ca69f9dc
Grigory Rylov <grishberg@gmail.com>
2019-12-06 09:07:02 +0300

    added properties

--- lib-android-layout-parser/src/main/java/com/github/grishberg/android/tools/layoutparser/viewnodes/ViewNode.java
+++ lib-android-layout-parser/src/main/java/com/github/grishberg/android/tools/layoutparser/viewnodes/ViewNode.java
@@ -1,56 +1,67 @@
 package com.github.grishberg.android.tools.layoutparser.viewnodes;
-import com.github.grishberg.layout.parser.*;
-import java.util.*;
+
+import com.github.grishberg.layout.parser.Node;
+import com.github.grishberg.layout.parser.NodeProperty;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ViewNode implements Node {
-	final Node parent; 
-	final String name;
-	final String hash;
-	int index = 0;
-	String id;
-	List<NodeProperty> properties = Collections.emptyList();
-	private LinkedList<ViewNode> children = new LinkedList<>();
+    final Node parent;
+    final String name;
+    final String hash;
+    int index = 0;
+    String id;
+    List<NodeProperty> properties = Collections.emptyList();
+    HashMap<String, NodeProperty> groupedProperties = new HashMap<>();
+    HashMap<String, NodeProperty> namedProperties = new HashMap<>();
+    private LinkedList<Node> children = new LinkedList<>();
 
-	public ViewNode(Node parent, String name, String hash) {
-		this.parent = parent;
-		this.name = name;
-		this.hash = hash;
-	}
+    public ViewNode(Node parent, String name, String hash) {
+        this.parent = parent;
+        this.name = name;
+        this.hash = hash;
+    }
 
-	@Override
-	public String name() {
-		return name;
-	}
+    @Override
+    public String name() {
+        return name;
+    }
 
-	@Override
-	public int childCount() {
-		// TODO: Implement this method
-		return children.size();
-	}
+    @Override
+    public int childCount() {
+        // TODO: Implement this method
+        return children.size();
+    }
 
-	void addChild(ViewNode node) {
-		children.add(node);
-	}
+    void addChild(ViewNode node) {
+        children.add(node);
+    }
 
-	@Override
-	public Node parent() {
-		return parent;
-	}
+    @Override
+    public Node parent() {
+        return parent;
+    }
 
-	@Override
-	public Node childAt(int p1) {
-		return children.get(p1);
-	}
+    @Override
+    public Node childAt(int p1) {
+        return children.get(p1);
+    }
 
-	@Override
-	public NodeProperty getProperty(String p1, String p2) {
-		// TODO: Implement this method
-		return null;
-	}
+    @Override
+    public NodeProperty getProperty(String name, String altNames) {
+        NodeProperty namedProperty = namedProperties.get(name);
+        return namedProperty;
+    }
 
-	@Override
-	public Enumeration<Node> children() {
-		// TODO: Implement this method
-		return null;
-	}
+    @Override
+    public List<Node> children() {
+        return children;
+    }
+
+    void setProperties(List<NodeProperty> properties) {
+
+    }
 }
